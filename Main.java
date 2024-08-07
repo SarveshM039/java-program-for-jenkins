e1014fbf07f313fe519f5be2f82a085d69dc5f38
SarveshM039 <sarveshm344@gmail.com>
2024-08-07 08:50:24 +0530

    added student class

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main{
         System.out.println("Output from java program");
         System.out.println("Hello Java");
         System.out.println(new Date());
+        Student student = new Student();
+        student.sayHello(); 
     }
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public Student(){
+        System.out.println("Hello form Student Class Constrotr");
+    }
+    public void sayHello(){
+        System.out.println("Hello from student class method");
+    }
+}
