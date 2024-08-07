53e815bd35d05c61979bedb17b0b35cf3030e6d2
SarveshM039 <sarveshm344@gmail.com>
2024-08-07 08:40:30 +0530

    Second Com it

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main{
     public static void main(String[] args){
         System.out.println("This is java program");
         System.out.println("Output from java program");
-        System.out.println("Hello");
+        System.out.println("Hello Java");
         System.out.println(new Date());
     }
 }
